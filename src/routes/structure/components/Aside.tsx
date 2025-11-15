06b420e9b2b269e52ab9cd0ddc04f5309c169645
Chetan Satpute <chetan.satpute2002@gmail.com>
2025-11-15 20:06:54 +0530

    feat(style): update Aside layout spacing and add rounded corners

--- src/routes/structure/components/Aside.tsx
+++ src/routes/structure/components/Aside.tsx
@@ -1,5 +1,5 @@
 function Aside() {
-  return <aside className="flex-1 overflow-auto bg-neutral-800"></aside>;
+  return <aside className="m-1 lg:m-2 rounded-lg flex-1 overflow-auto bg-neutral-800"></aside>;
 }
 
 export default Aside;
