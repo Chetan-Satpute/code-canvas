e5a80b9c49f29b38b5524cf0c710896d604ae8b6
Chetan Satpute <chetan.satpute2002@gmail.com>
2026-02-18 16:17:33 +0530

    remove auto play button from controls

--- src/routes/structure/algorithm/components/Controls.tsx
+++ src/routes/structure/algorithm/components/Controls.tsx
@@ -1,28 +1,13 @@
-import { Play, RedoDot, Square, X } from 'lucide-react';
+import { RedoDot, X } from 'lucide-react';
 
 import { useControls } from '../hooks/controls';
 import ControlButton from './ControlButton';
 
 function Controls() {
   const {
-    isAutoplaying,
     handleClose,
     handleNextStep,
-    handleStart,
-    handleStop,
   } = useControls();
-
-  if (isAutoplaying) {
-    return (
-      <div className="flex">
-        <ControlButton
-          label="Stop"
-          icon={<Square size={16} />}
-          onClick={handleStop}
-        />
-      </div>
-    );
-  }
 
   return (
     <div className="flex">
@@ -32,14 +17,6 @@ function Controls() {
         icon={<X size={16} />}
         onClick={() => handleClose()}
       />
-
-      <ControlButton
-        label="Start"
-        icon={<Play size={16} />}
-        onClick={handleStart}
-        className="rounded-none"
-      />
-
       <ControlButton
         className="rounded-l-none"
         label="Next Step"
