9337c81f6f671cadb3ea80bba0912ffad31648e2
Chetan Satpute <chetan.satpute2002@gmail.com>
2026-09-11 17:46:27 +0530

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,22 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+
+  plugins: [
+    '@trivago/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+  ],
+
+  importOrder: ['<THIRD_PARTY_MODULES>', '^#.*', '^[./]'],
+
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  importOrderCaseInsensitive: true,
+};
+
+export default config;
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-background flex h-screen w-screen items-center justify-center">
+      <h1 className="text-foreground font-en-display text-xl font-bold">
+        Code Canvas
+      </h1>
+    </div>
+  );
+}
+
+export default App;
